34e2e6379d0257652963de3323990ae1eafe6e1e
Jigar Lodaya <jigar@bravocode.in>
2016-12-28 23:31:49 +0530

    New Updates Updated Projects. Updated Emplyment. UI Changes.

---
+++ static/js/resume.js
@@ -0,0 +1,23 @@
+$('.three').css({
+    position: 'fixed',
+    // top: '20',
+   //  right: '40px'
+});
+// var fixmeTop = $('.three').offset().top-40;
+// console.log(fixmeTop);
+// $(window).scroll(function() {
+//    var currentScroll = $(window).scrollTop();
+//    console.log(currentScroll);
+//    if (currentScroll >= fixmeTop) {
+//        $('.three').css({
+//            position: 'fixed',
+//            top: '20',
+//           //  right: '40px'
+//        });
+//    } else {
+//        $('.three').css({
+//            position: 'static'
+//        });
+//    }
+//    console.log("test");
+// });
